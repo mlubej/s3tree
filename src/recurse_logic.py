c99a44426bf1aef527cbd015545b20b0ce274c75
Matic Lubej <matic.lubej@sinergise.com>
2022-06-15 15:03:39 +0200

    improve logic, dynamically print content, dont use tree, color the output, fix bugs

--- src/recurse_logic.py
+++ src/recurse_logic.py
@@ -1,9 +1,15 @@
 import re
-import subprocess
 from typing import List
 
 import boto3
 import click
+
+SPACE = "    "
+BRANCH = "│   "
+TEE = "├── "
+LAST = "└── "
+DIR_COLOR = "\033[94m"
+END_COLOR = "\033[0m"
 
 
 def parse_s3url(s3url: str) -> List[str]:
@@ -14,48 +20,60 @@ def parse_s3url(s3url: str) -> List[str]:
     return s3url.split("/", maxsplit=1)
 
 
-def process_list(object_list: List[str], pattern: str, ignore_pattern: str) -> List[str]:
-    if pattern != "":
-        object_list = [obj for obj in object_list if re.search(pattern, obj.split("/")[-1])]
+def process_list(object_list: List[str], pattern: str, i_pattern: str) -> List[str]:
+    new_object_list = []
+    for obj in object_list:
+        if obj.endswith("/"):
+            new_object_list.append(obj)
+            continue
 
-    if ignore_pattern != "":
-        object_list = [obj for obj in object_list if not re.search(ignore_pattern, obj.split("/")[-1])]
+        f_name = obj.split("/")[-1]
+        pattern_condition = True if pattern == "" else re.search(pattern, f_name)
+        i_pattern_condition = False if i_pattern == "" else re.search(i_pattern, f_name)
 
-    return object_list
+        if pattern_condition and not i_pattern_condition:
+            new_object_list.append(obj)
+    return new_object_list
 
 
-def recursive_listdir(
+def tree(
     client: boto3.client,
     bucket: str,
     depth: int,
     limit: int,
     pattern: str,
-    ignore_pattern: str,
+    i_pattern: str,
+    prune: bool,
     prefix: str = "",
-    global_list: List[str] = [],
-    global_level: int = 0,
-) -> List[str]:
+    extension: str = "",
+    level: int = 0,
+) -> None:
 
-    response = client.list_objects_v2(Bucket=bucket, Prefix=prefix, Delimiter="/", MaxKeys=limit)
-
-    if "Contents" in response:
-        object_list = [c["Key"] for c in response["Contents"] if c != prefix]
-        object_list = process_list(object_list, pattern, ignore_pattern)
-        global_list.extend(object_list)
-
+    response = client.list_objects_v2(Bucket=bucket, Prefix=prefix, Delimiter="/", MaxKeys=limit + 1)
+    object_list = []
     if "CommonPrefixes" in response:
-        object_list = [r["Prefix"] for r in response["CommonPrefixes"]]
-        object_list = process_list(object_list, pattern, ignore_pattern)
-        global_list.extend(object_list)
+        object_list.extend([r["Prefix"] for r in response["CommonPrefixes"]])
+    if "Contents" in response:
+        object_list.extend([c["Key"] for c in response["Contents"] if c["Key"] != prefix])
+    object_list = process_list(object_list, pattern, i_pattern)
 
-        for entry in object_list:
-            if global_level < depth - 1:
-                deeper_list = recursive_listdir(
-                    client, bucket, depth, limit, pattern, ignore_pattern, entry, global_list, global_level + 1
-                )
-                global_list.extend(deeper_list)
+    if response["IsTruncated"]:
+        object_list.append(prefix + "...")
 
-    return global_list
+    pointers = [TEE] * (len(object_list) - 1) + [LAST]
+    for pointer, path in zip(pointers, object_list):
+        out = path.replace(prefix, "")
+        is_dir = out.endswith("/")
+        out = f"{DIR_COLOR}{out}{END_COLOR}" if is_dir else out
+
+        if prune and level == depth - 1 and is_dir:
+            continue
+        else:
+            print(extension + pointer + out)
+
+        if level < depth - 1:
+            pre_ext = BRANCH if pointer == TEE else SPACE
+            tree(client, bucket, depth, limit, pattern, i_pattern, prune, path, pre_ext + extension, level + 1)
 
 
 @click.command()
@@ -65,7 +83,7 @@ def recursive_listdir(
 @click.option("--limit", "-l", default=10, type=int, help="Limit results per AWS request in recursion step.")
 @click.option("--pattern", "-P", type=str, default="", help="List only those files that match the wild-card pattern.")
 @click.option(
-    "--ignore_pattern", "-I", type=str, default="", help="Do not list those files that match the wild-card pattern."
+    "--i_pattern", "-I", type=str, default="", help="Do not list those files that match the wild-card pattern."
 )
 @click.option(
     "--prune",
@@ -73,29 +91,14 @@ def recursive_listdir(
     default=False,
     help="Makes tree prune empty directories from the output, useful when used in conjunction with -P or -I.",
 )
-@click.option(
-    "--kwargs",
-    default="",
-    type=str,
-    help=(
-        "Extra parameters provided to the `tree` command. Only make sense for the usecase with the `--fromfile .`"
-        " option."
-    ),
-)
-def s3tree(
-    s3url: str, depth: int, profile: str, limit: int, pattern: str, ignore_pattern: str, prune: bool, kwargs: List[str]
-) -> None:
+def s3tree(s3url: str, depth: int, profile: str, limit: int, pattern: str, i_pattern: str, prune: bool) -> None:
 
     bucket_name, prefix, *_ = parse_s3url(s3url)
     session = boto3.Session(profile_name=profile)
     client = session.client("s3")
 
-    object_list = recursive_listdir(client, bucket_name, depth, limit, pattern, ignore_pattern, prefix)
-    object_list = [key[len(prefix) :] for key in object_list]
-    output = "\n".join(object_list)
-
-    prune_option = "--prune" if prune else ""
-    subprocess.call(f"echo '{output}' | tree --fromfile . {prune_option} {kwargs}", shell=True)
+    print(f"{DIR_COLOR}{s3url}{END_COLOR}")
+    tree(client, bucket_name, depth, limit, pattern, i_pattern, prune, prefix)
 
 
 if __name__ == "__main__":
